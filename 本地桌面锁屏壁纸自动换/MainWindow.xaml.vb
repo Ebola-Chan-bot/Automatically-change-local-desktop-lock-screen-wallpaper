bb72e67c564a50c995836fae466bf88a696d3120
埃博拉酱 <vhtmfu@outlook.com>
2023-05-07 20:47:14 +0800

    桌面锁屏取设

--- 本地桌面锁屏壁纸自动换/MainWindow.xaml.vb
+++ 本地桌面锁屏壁纸自动换/MainWindow.xaml.vb
@@ -1,17 +1,9 @@
 ﻿Imports Microsoft.Win32
 Imports 本地桌面锁屏壁纸自动换.My
 Imports System.IO
+Imports System.Windows.Forms
 
 Class MainWindow
-	'TileWallpaper*23+WallPaper
-	Enum 桌面壁纸样式 As Byte
-		填充 = 10
-		适应 = 6
-		拉伸 = 2
-		平铺 = 23
-		居中 = 0
-		跨区 = 22
-	End Enum
 
 	Sub New()
 
@@ -31,41 +23,36 @@ Class MainWindow
 		锁屏_更换周期.SelectedIndex = Settings.锁屏轮换周期
 		桌面_浏览图集.Content = Settings.所有桌面目录
 		锁屏_浏览图集.Content = Settings.所有锁屏目录
-		Dim 当前桌面壁纸路径 As String() = Nothing
-		If Forms.Screen.AllScreens.Length > 1 Then
-			Static Desktop As RegistryKey = Registry.CurrentUser.OpenSubKey("Control Panel\Desktop")
-			Dim TileWallpaper As String = Desktop.GetValue("TileWallpaper")
-			Dim WallpaperStyle As String = Desktop.GetValue("WallpaperStyle")
-			Select Case TileWallpaper * 23 + WallpaperStyle
-				Case 桌面壁纸样式.填充, 桌面壁纸样式.适应, 桌面壁纸样式.拉伸
-					当前桌面壁纸路径 = Directory.GetFiles(Settings.当前桌面目录, "Transcoded_*")
-				Case 桌面壁纸样式.平铺, 桌面壁纸样式.居中, 桌面壁纸样式.跨区
-					当前桌面壁纸路径 = Directory.GetFiles(Settings.当前桌面目录, "TranscodedWallpaper")
-				Case Else
-					桌面图片错误.Text = $"桌面壁纸样式解析失败：TileWallpaper={TileWallpaper}, WallpaperStyle={WallpaperStyle}"
-			End Select
-		Else
-			当前桌面壁纸路径 = Directory.GetFiles(Path.Combine(Settings.当前桌面目录, "CachedFiles"), "CachedImage_*.jpg")
-		End If
-		If 当前桌面壁纸路径 IsNot Nothing Then
-			桌面壁纸列表.ItemsSource = 当前桌面壁纸路径
-		End If
+		桌面壁纸列表.ItemsSource = 桌面锁屏取设.各屏壁纸()
 		Dim 锁屏文件 As String = Nothing
 		Dim 最新修改 As Date = Date.MinValue
-		For Each 目录 As String In Directory.GetDirectories(Settings.当前锁屏目录)
-			Dim 文件 As String = Path.Combine(目录, "LockScreen.jpg")
-			If File.Exists(文件) Then
-				Dim 修改 As Date = File.GetLastWriteTime(文件)
-				If 修改 > 最新修改 Then
-					锁屏文件 = 文件
-					最新修改 = 修改
+		Try
+			For Each 目录 As String In Directory.GetDirectories(Settings.当前锁屏目录)
+				Dim 文件 As String = Path.Combine(目录, "LockScreen.jpg")
+				If File.Exists(文件) Then
+					Dim 修改 As Date = File.GetLastWriteTime(文件)
+					If 修改 > 最新修改 Then
+						锁屏文件 = 文件
+						最新修改 = 修改
+					End If
 				End If
+			Next
+			If 锁屏文件 Is Nothing Then
+				Throw New FileNotFoundException($"【{Settings.当前锁屏目录}】中没有找到锁屏图片")
+			Else
+				锁屏_当前图片.Source = New BitmapImage(New Uri(锁屏文件))
 			End If
-		Next
-		If 锁屏文件 Is Nothing Then
-			锁屏图片错误.Text = "没有找到锁屏图片"
-		Else
-			锁屏_当前图片.Source = New BitmapImage(New Uri(锁屏文件))
+		Catch ex As Exception
+			锁屏图片错误.Text = $"{ex.GetType} {vbCrLf & ex.Message}"
+		End Try
+	End Sub
+
+	Private 文件浏览对话框 As New FolderBrowserDialog
+
+	Private Sub 桌面_浏览_Click(sender As Object, e As RoutedEventArgs) Handles 桌面_浏览.Click
+		Dim 对话框结果 As DialogResult = 文件浏览对话框.ShowDialog
+		If 对话框结果 = Forms.DialogResult.OK AndAlso Directory.Exists(文件浏览对话框.SelectedPath) Then
+
 		End If
 	End Sub
 End Class
---
+++ 桌面锁屏取设/桌面.vb
@@ -0,0 +1,48 @@
+﻿Imports System.IO
+Imports Microsoft.Win32
+
+Public Structure 屏幕壁纸
+	Property 设备名称 As String
+	Property 壁纸路径 As String
+End Structure
+
+Enum 桌面壁纸样式 As Byte
+	填充 = 10
+	适应 = 6
+	拉伸 = 2
+	平铺 = 23
+	居中 = 0
+	跨区 = 22
+End Enum
+
+Public Module 桌面
+	Function 各屏壁纸(搜索路径 As String) As 屏幕壁纸()
+		Dim 所有屏幕 As Screen() = Screen.AllScreens()
+		所有屏幕(0).
+		Dim 当前桌面壁纸路径 As String() = Nothing
+		Try
+			If Screen.AllScreens.Length > 1 Then
+				Static Desktop As RegistryKey = Registry.CurrentUser.OpenSubKey("Control Panel\Desktop")
+				Dim TileWallpaper As String = Desktop.GetValue("TileWallpaper")
+				Dim WallpaperStyle As String = Desktop.GetValue("WallpaperStyle")
+				Select Case TileWallpaper * 23 + WallpaperStyle
+					Case 桌面壁纸样式.填充, 桌面壁纸样式.适应, 桌面壁纸样式.拉伸
+						当前桌面壁纸路径 = Directory.GetFiles(搜索路径, "Transcoded_*")
+					Case 桌面壁纸样式.平铺, 桌面壁纸样式.居中, 桌面壁纸样式.跨区
+						当前桌面壁纸路径 = Directory.GetFiles(搜索路径, "TranscodedWallpaper")
+					Case Else
+						Throw New Exception($"桌面壁纸样式解析失败：TileWallpaper={TileWallpaper}, WallpaperStyle={WallpaperStyle}")
+				End Select
+			Else
+				当前桌面壁纸路径 = Directory.GetFiles(Path.Combine(搜索路径, "CachedFiles"), "CachedImage_*.jpg")
+			End If
+			If 当前桌面壁纸路径 IsNot Nothing Then
+			End If
+		Catch ex As Exception
+		End Try
+	End Function
+
+	Function 各屏壁纸() As 屏幕壁纸()
+		Return 各屏壁纸(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Microsoft\Windows\Themes"))
+	End Function
+End Module
