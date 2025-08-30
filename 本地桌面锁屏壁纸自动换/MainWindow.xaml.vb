b11df545052d61b63c3ae373f9a6e530aa10eebd
埃博拉酱 <vhtmfu@outlook.com>
2025-08-30 22:45:34 +0800

    完全改用窗体目录浏览对话框

--- 本地桌面锁屏壁纸自动换/MainWindow.xaml.vb
+++ 本地桌面锁屏壁纸自动换/MainWindow.xaml.vb
@@ -2,7 +2,6 @@
 Imports System.IO
 Imports System.Security.Principal
 Imports Microsoft.Win32
-Imports Windows.Storage
 Imports 桌面壁纸取设
 
 Class MainWindow
@@ -79,8 +78,8 @@ Class MainWindow
 		Sub New(父 As 桌面呈现结构)
 			Me.父 = 父
 		End Sub
-		Public Async Sub Execute(parameter As Object) Implements ICommand.Execute
-			父.图集目录 = (Await 父.主窗口.运行时目录浏览对话框.PickSingleFolderAsync)?.Path
+		Public Sub Execute(parameter As Object) Implements ICommand.Execute
+			父.图集目录 = If(父.主窗口.目录浏览对话框.ShowDialog = Forms.DialogResult.OK, 父.主窗口.目录浏览对话框.SelectedPath, Nothing)
 		End Sub
 
 		Public Function CanExecute(parameter As Object) As Boolean Implements ICommand.CanExecute
@@ -242,27 +241,19 @@ Class MainWindow
 			更新当前锁屏()
 		End If
 	End Sub
-	ReadOnly 运行时目录浏览对话框 As New Pickers.FolderPicker
-	ReadOnly 窗体目录浏览对话框 As New Forms.FolderBrowserDialog
-	Async Function 浏览目录() As Task(Of String)
-		If 提权 Then
-			Return If(窗体目录浏览对话框.ShowDialog = Forms.DialogResult.OK, 窗体目录浏览对话框.SelectedPath, Nothing)
-		Else
-			Return (Await 运行时目录浏览对话框.PickSingleFolderAsync)?.Path
-		End If
-	End Function
-	Private Async Sub 桌面_浏览图集_Click(sender As Object, e As RoutedEventArgs) Handles 桌面_浏览图集.Click
-		Dim 目录 As String = Await 浏览目录()
-		If 目录 IsNot Nothing Then
-			默认桌面.SetValue("图集目录", 目录)
-			桌面_图集目录.Text = 目录
+
+	'不能用Windows.Storage.Pickers.FolderPicker，因为它不能在提权进程中使用
+	ReadOnly 目录浏览对话框 As New Forms.FolderBrowserDialog
+	Private Sub 桌面_浏览图集_Click(sender As Object, e As RoutedEventArgs) Handles 桌面_浏览图集.Click
+		If 目录浏览对话框.ShowDialog = Forms.DialogResult.OK Then
+			默认桌面.SetValue("图集目录", 目录浏览对话框.SelectedPath)
+			桌面_图集目录.Text = 目录浏览对话框.SelectedPath
 		End If
 	End Sub
-	Private Async Sub 锁屏_浏览图集_Click(sender As Object, e As RoutedEventArgs) Handles 锁屏_浏览图集.Click
-		Dim 目录 As String = Await 浏览目录()
-		If 目录 IsNot Nothing Then
-			默认锁屏.SetValue("图集目录", 目录)
-			锁屏_图集目录.Text = 目录
+	Private Sub 锁屏_浏览图集_Click(sender As Object, e As RoutedEventArgs) Handles 锁屏_浏览图集.Click
+		If 目录浏览对话框.ShowDialog = Forms.DialogResult.OK Then
+			默认锁屏.SetValue("图集目录", 目录浏览对话框.SelectedPath)
+			锁屏_图集目录.Text = 目录浏览对话框.SelectedPath
 		End If
 	End Sub
 	Private Sub 桌面_立即更换_Click(sender As Object, e As RoutedEventArgs) Handles 桌面_立即更换.Click
@@ -378,10 +369,6 @@ Class MainWindow
 		AddHandler 桌面_更换周期.SelectionChanged, AddressOf 桌面_更换周期_SelectionChanged
 		AddHandler 锁屏_更换周期.SelectionChanged, AddressOf 锁屏_更换周期_SelectionChanged
 		AddHandler 锁屏_立即更换.Click, AddressOf 换锁屏
-
-		'只能在Loaded中初始化，因为构造阶段窗口还没有句柄
-		winrt.Interop.InitializeWithWindow.Initialize(运行时目录浏览对话框, New Interop.WindowInteropHelper(Me).Handle)
-		运行时目录浏览对话框.FileTypeFilter.Add("*")
 
 		If 桌面未加载 Then
 			更新当前桌面()
